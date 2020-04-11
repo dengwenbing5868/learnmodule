350478961c712d3ddae242413177d6a7feb5cb6f
SuperSaiyan <809605798@qq.com>
2020-04-11 10:48:47 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+"net/http"
+"os"
+"time"
+
+"github.com/gorilla/mux"
+log "github.com/sirupsen/logrus"
+)
+
+func main() {
+log.SetFormatter(&log.JSONFormatter{})
+log.SetOutput(os.Stdout)
+
+mtx := mux.NewRouter()
+mtx.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+log.WithField("user-agent", r.UserAgent()).Info("accessed home page")
+_, _ = w.Write([]byte("Hello World!"))
+})
+
+srv := &http.Server{
+Handler:      mtx,
+Addr:         "127.0.0.1:8000",
+WriteTimeout: 10 * time.Second,
+ReadTimeout:  10 * time.Second,
+}
+
+log.Fatal(srv.ListenAndServe())
+}
